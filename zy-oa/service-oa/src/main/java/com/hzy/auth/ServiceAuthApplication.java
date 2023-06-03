66a3a7d587f14b4fca697f66922931647ff6c583
zxwyhzy <2791517764@qq.com>
2023-06-03 23:55:23 +0800

    修改：修改扫描规则

--- zy-oa/service-oa/src/main/java/com/hzy/auth/ServiceAuthApplication.java
+++ zy-oa/service-oa/src/main/java/com/hzy/auth/ServiceAuthApplication.java
@@ -2,6 +2,7 @@ package com.hzy.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @title: ServiceAuthApplication
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@ComponentScan("com.hzy") // 配置统一扫描这个包下的内容
 public class ServiceAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAuthApplication.class);
