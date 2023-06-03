961830c75f4b897e2f9b069bf2775785c66f1a70
zxwyhzy <2791517764@qq.com>
2023-06-03 23:15:51 +0800

    添加：添加统一结果返回对象

---
+++ zy-oa/common/common-util/src/main/java/com/hzy/common/result/Result.java
@@ -0,0 +1,94 @@
+package com.hzy.common.result;
+
+/**
+ * @title: Result 全局统一返回结果类
+ * @Author zxwyhzy
+ * @Date: 2023/6/3 23:05
+ * @Version 1.0
+ */
+
+import lombok.Data;
+
+
+@Data
+public class Result<T> {
+
+    //返回码
+    private Integer code;
+
+    //返回消息
+    private String message;
+
+    //返回数据
+    private T data;
+
+    public Result() {
+    }
+
+    // 返回数据
+    protected static <T> Result<T> build(T data) {
+        Result<T> result = new Result<T>();
+        if (data != null) {
+            result.setData(data);
+        }
+        return result;
+    }
+
+    public static <T> Result<T> build(T body, Integer code, String message) {
+        Result<T> result = build(body);
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
+    public static <T> Result<T> build(T body, ResultCodeEnum resultCodeEnum) {
+        Result<T> result = build(body);
+        result.setCode(resultCodeEnum.getCode());
+        result.setMessage(resultCodeEnum.getMessage());
+        return result;
+    }
+
+    public static <T> Result<T> ok() {
+        return Result.ok(null);
+    }
+
+    /**
+     * 操作成功
+     *
+     * @param data baseCategory1List
+     * @param <T>
+     * @return
+     */
+    public static <T> Result<T> ok(T data) {
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.SUCCESS);
+    }
+
+
+    /**
+     * 操作失败
+     *
+     * @param data
+     * @param <T>
+     * @return
+     */
+
+    public static <T> Result<T> fail(T data) {
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.FAIL);
+    }
+
+    public static <T> Result<T> fail() {
+        return Result.fail(null);
+    }
+
+    public Result<T> message(String msg) {
+        this.setMessage(msg);
+        return this;
+    }
+
+    public Result<T> code(Integer code) {
+        this.setCode(code);
+        return this;
+    }
+}
---
+++ zy-oa/common/common-util/src/main/java/com/hzy/common/result/ResultCodeEnum.java
@@ -0,0 +1,32 @@
+package com.hzy.common.result;
+
+/**
+ * @title: ResultCodeEnum 统一返回结果状态信息类
+ * @Author zxwyhzy
+ * @Date: 2023/6/3 23:04
+ * @Version 1.0
+ */
+
+import lombok.Getter;
+
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200,"成功"),
+    FAIL(201, "失败"),
+    SERVICE_ERROR(2012, "服务异常"),
+    DATA_ERROR(204, "数据异常"),
+
+    LOGIN_AUTH(208, "未登陆"),
+    PERMISSION(209, "没有权限")
+    ;
+
+    private Integer code;
+
+    private String message;
+
+    private ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
