f739c3028576b9703b584f3c237cd116ae2167c3
zxwyhzy <2791517764@qq.com>
2023-06-04 20:49:22 +0800

    修改：修改了vue.config.js里配置的请求路径;修改了request.js返回拦截器的成功判定;修改了路由； 添加了角色管理的api和页面

---
+++ zy-oa-admin/src/api/system/sysRole.js
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/system/sysRole'
+export default {
+  getPageList(current, limit, searchObj) {
+    return request({
+      url: `${api_name}/${current}/${limit}`,
+      method: 'get',
+      // 如果普通对象参数写法， params:对象参数名称
+      // 如果使用json格式传递， data: 对象参数名称
+      params: searchObj
+    })
+  },
+  remove(id) {
+    return request({
+      url: `${api_name}/remove/${id}`,
+      method: 'delete'
+    })
+  },
+  save(sysRole) {
+    return request({
+      url: `${api_name}/save`,
+      method: 'post',
+      data: sysRole
+    })
+  },
+  update(sysRole) {
+    return request({
+      url: `${api_name}/update`,
+      method: 'put',
+      data: sysRole
+    })
+  }
+}
